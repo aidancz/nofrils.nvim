8fea25601c62e0bf36a456d4420f08cc214dd9a1
aidancz <aidanczhai@gmail.com>
2024-11-16 22:36:14 +0800

    xresources optimization

--- lua/nofrils/xresources.lua
+++ lua/nofrils/xresources.lua
@@ -1,33 +1,65 @@
 -- https://github.com/nekonako/xresources-nvim/blob/master/lua/xresources.lua
+-- https://github.com/martineausimon/nvim-xresources/blob/main/lua/nvim-xresources/system.lua
 
-local function get_xresources_color (c)
+local H = {}
+
+H.get_xresources_color = function(c)
 	local command = io.popen('xrdb -query | grep ' .. c .. ' -m 1 | cut -f 2')
 	local color = command:read("*l")
 	return color
 end
 
-local x = {
-	fg  = get_xresources_color('foreground');
-	bg  = get_xresources_color('background');
-	c0  = get_xresources_color('color0');
-	c1  = get_xresources_color('color1');
-	c2  = get_xresources_color('color2');
-	c3  = get_xresources_color('color3');
-	c4  = get_xresources_color('color4');
-	c5  = get_xresources_color('color5'),
-	c6  = get_xresources_color('color6');
-	c7  = get_xresources_color('color7');
-	c8  = get_xresources_color('color8');
-	c9  = get_xresources_color('color9');
-	c10 = get_xresources_color('color10');
-	c11 = get_xresources_color('color11');
-	c12 = get_xresources_color('color12');
-	c13 = get_xresources_color('color13');
-	c14 = get_xresources_color('color14');
-	c15 = get_xresources_color('color15');
+H.color_name = {
+	'foreground',
+	'background',
+	'color0',
+	'color1',
+	'color2',
+	'color3',
+	'color4',
+	'color5',
+	'color6',
+	'color7',
+	'color8',
+	'color9',
+	'color10',
+	'color11',
+	'color12',
+	'color13',
+	'color14',
+	'color15',
 
-	black_alt = get_xresources_color('color_black_alt');
-	white_alt = get_xresources_color('color_white_alt');
+	'color_black_alt',
+	'color_white_alt',
 }
 
-return x
+
+
+local M = {}
+
+M.path_cache = vim.fn.stdpath('cache') .. '/xresources.lua'
+
+M.path_xresources = os.getenv("HOME") .. '/.Xresources'
+
+M.update_cache = function()
+	if vim.fn.getftime(M.path_cache) < vim.fn.getftime(M.path_xresources)
+	then
+		local color_def = {}
+
+		for _, c in ipairs(H.color_name)
+		do
+			color_def[c] = H.get_xresources_color(c)
+		end
+
+		local file_cache = io.open(M.path_cache, "w")
+		if file_cache then
+			file_cache:write("return\n" .. vim.inspect(color_def))
+			file_cache:close()
+		end
+	end
+end
+
+
+
+M.update_cache()
+return dofile(M.path_cache)
